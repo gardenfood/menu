2682de45fcb807148ea4edbcd6372925562fca0b
gardenfood <gardenfood88@gmail.com>
2026-07-21 00:05:24 +0300

    Create sw.js

---
+++ admin/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'gardenfood-admin-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  '../settings.json',
+  '../menu.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
